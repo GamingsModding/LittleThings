7573700fa75efb9fce53aedca3b35b53866e5270
Dominic Carrington <carringtondominic@gmail.com>
2016-07-13 10:17:16 +0100

    Added recipe for Redstone clock

--- src/main/java/com/gamingsmod/littlethings/automation/addition/RedstoneClock.java
+++ src/main/java/com/gamingsmod/littlethings/automation/addition/RedstoneClock.java
@@ -3,8 +3,13 @@ package com.gamingsmod.littlethings.automation.addition;
 import com.gamingsmod.littlethings.automation.block.BlockRedstoneClock;
 import com.gamingsmod.littlethings.automation.tileentity.TileEntityRedstoneClock;
 import com.gamingsmod.littlethings.base.block.ModBlock;
+import com.gamingsmod.littlethings.base.helper.RecipeHelper;
 import com.gamingsmod.littlethings.base.lib.LibMisc;
 import com.gamingsmod.littlethings.base.loader.Addition;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -18,5 +23,13 @@ public class RedstoneClock extends Addition
         redstone_clock = new BlockRedstoneClock();
 
         GameRegistry.registerTileEntity(TileEntityRedstoneClock.class, LibMisc.PREFIX_MOD + "redstone_clock");
+    }
+
+    @Override
+    public void init(FMLInitializationEvent e)
+    {
+        RecipeHelper.addShapelessRecipe(redstone_clock,
+                new ItemStack(Blocks.REDSTONE_BLOCK),
+                new ItemStack(Items.CLOCK));
     }
 }
