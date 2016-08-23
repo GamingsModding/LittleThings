c0793b3ff87705662e1d7ec9cf6fc55efd1e5f20
Dominic Carrington <carringtondominic@gmail.com>
2016-08-23 08:01:19 +0100

    Update to 1.10.2

--- src/main/java/com/gamingsmod/littlethings/base/lib/LibMisc.java
+++ src/main/java/com/gamingsmod/littlethings/base/lib/LibMisc.java
@@ -6,7 +6,7 @@ public class LibMisc
     public static final String MOD_NAME = "Little Things";
     public static final String BUILD = "GRADLE:BUILD";
     public static final String VERSION = "GRADLE:VERSION-" + BUILD;
-    public static final String DEPENDENCIES = "required-after:Forge@[12.18.0.2000,);";
+    public static final String DEPENDENCIES = "required-after:Forge@[12.18.1.2053,);";
     public static final String PREFIX_MOD = MOD_ID.toLowerCase() + ":";
     public static final String MOD_ID_LOWER = MOD_ID.toLowerCase();
 
